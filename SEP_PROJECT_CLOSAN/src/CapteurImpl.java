e179a2d5ba3b45bb116510854f5e7921afd86e2d
14008757 <14008757@e008m06.istic.univ-rennes1.fr>
2016-11-21 11:31:30 +0100

    [Edit] [Ajout methodes] Canal, CapteurImpl, Capteur, Subjetc

--- SEP_PROJECT_CLOSAN/src/CapteurImpl.java
+++ SEP_PROJECT_CLOSAN/src/CapteurImpl.java
@@ -1,6 +1,31 @@
 import interfacePackage.Capteur;
+import interfacePackage.Observer;
 
 
 public class CapteurImpl implements Capteur {
+
+	@Override
+	public void attach(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void detach(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- SEP_PROJECT_CLOSAN/src/interfacePackage/Subject.java
+++ SEP_PROJECT_CLOSAN/src/interfacePackage/Subject.java
@@ -2,4 +2,8 @@ package interfacePackage;
 
 public interface Subject {
 
+	public void attach(Observer o);
+	
+	public void detach(Observer o);
+	
 }
